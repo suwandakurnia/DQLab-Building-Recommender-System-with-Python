2982b9b88a794b124fa336bfc96dfc33cb4b3c61
Suwanda Kurnia <63093353+suwandakurnia@users.noreply.github.com>
2022-04-15 13:55:55 +0700

    Create 17 Pertanyaan 3: Bagaimana cara membuat fungsi weighted formula?.py

---
+++ 17 Pertanyaan 3: Bagaimana cara membuat fungsi weighted formula?.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+pd.set_option('display.max_columns', None)
+
+movie_df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/title.basics.tsv', sep='\t')
+rating_df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/title.ratings.tsv', sep='\t')
+
+movie_df = movie_df.loc[(movie_df['primaryTitle'].notnull()) & (movie_df['originalTitle'].notnull())]
+movie_df = movie_df.loc[movie_df['genres'].notnull()]
+movie_df['startYear'] = movie_df['startYear'].replace('\\N', np.nan)
+movie_df['startYear'] = movie_df['startYear'].astype('float64')
+movie_df['endYear'] = movie_df['endYear'].replace('\\N', np.nan)
+movie_df['endYear'] = movie_df['endYear'].astype('float64')
+movie_df['runtimeMinutes'] = movie_df['runtimeMinutes'].replace('\\N', np.nan)
+movie_df['runtimeMinutes'] = movie_df['runtimeMinutes'].astype('float64')
+
+def transform_to_list(x):
+    if ',' in x: 
+        return x.split(',')
+    else: 
+        return []
+
+movie_df['genres'] = movie_df['genres'].apply(lambda x: transform_to_list(x))
+
+movie_rating_df = pd.merge(movie_df, rating_df, on='tconst', how='inner')
+movie_rating_df = movie_rating_df.dropna(subset=['startYear','runtimeMinutes'])
+
+def imdb_weighted_rating(df, var=0.8):
+    v = df['numVotes']
+    R = df['averageRating']
+    C = df['averageRating'].mean()
+    m = df['numVotes'].quantile(var)
+    df['score'] = (v/(m+v))*R + (m/(m+v))*C #Rumus IMDb 
+    return df['score']
+    
+imdb_weighted_rating(movie_rating_df)
+
+#melakukan pengecekan dataframe
+print(movie_rating_df.head())
