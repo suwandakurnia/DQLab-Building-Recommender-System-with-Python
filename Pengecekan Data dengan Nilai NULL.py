37ffd9e36c4ddd942afbc839d6a4b388c3377754
Suwanda Kurnia <63093353+suwandakurnia@users.noreply.github.com>
2022-04-15 13:23:17 +0700

    Create Pengecekan Data dengan Nilai NULL.py

---
+++ Pengecekan Data dengan Nilai NULL.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+pd.set_option('display.max_columns', None)
+
+movie_df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/title.basics.tsv', sep='\t')
+
+print(movie_df.isnull().sum())
